d9b4e2b3aa68e267eb55cef46ecf3a62e2df4bb0
ChrisScheidig <c@scheidig.net>
2018-11-29 18:12:59 +0100

    Updaded language files for skellboy

--- js/skellboytranslation.js
+++ js/skellboytranslation.js
@@ -3,33 +3,39 @@
 
 var lang_ger = {
 	lang_skelltext_1:       {type:"innerHTML", text:`
-Manchmal, wenn sich sonst niemand dafür bereit erklärt, braucht es einen alten, wiederauferstandenen
-Helden um die Welt zu retten! Vernichte deine Gegner und mach dir ihr Innerstes zu Nutze - wortwörtlich,
-denn mit den Körperteilen und Waffen deiner Gegner kannst du Skippy aufrüsten!
+Manchmal, wenn sich sonst niemand dafür bereit erklärt, braucht es einen alten, <strong>wiederauferstandenen
+Helden</strong> um die Welt zu retten! Vernichte deine Gegner und mach dir ihr Innerstes zu Nutze - wortwörtlich,
+denn mit den <strong>Körperteilen und Waffen deiner Gegner kannst du Skippy aufrüsten</strong>!
 <br><br>
-In diesem rechtwinkligen, actionbepackten Papp-Abenteuer hilfst du Skippy, dem eckigen Skelett,
-Kastputins endlose Armeen untoter und bösartiger Unholde zu bekämpfen!
-<br><br>
-Skellboy wird mit Unity entwickelt und ist für einen Release Mitte 2019 für Windows,
-MaxOS und Linux geplant!
+In diesem rechtwinkligen, actionbepackten Papp-Abenteuer hilfst du <strong>Skippy</strong>, dem eckigen Skelett,
+<strong>Kastputins</strong> endlose Armeen untoter und bösartiger Unholde zu bekämpfen!
 	` },
 	lang_skelltext_release: {type:"innerHTML", text:"Mitte 2019"},	
+	lang_skelltext_and: {type:"innerHTML", text:"und"},		
+	lang_skelltext_pub: {type:"innerHTML", text:"Switch Version veröffentlicht von"},	
+	lang_skelltext_follow: {type:"innerHTML", text:"Folge uns!"},	
+	lang_skelltext_follow2: {type:"innerHTML", text:"Tritt unserem <a href='https://discord.gg/8eWHk53'>discord</a>  bei und teile alle deine <strong>Knochenwitze</strong> mit uns!"},	
+	lang_skelltext_follow3: {type:"innerHTML", text:"oder"},		
+	lang_skelltext_follow4: {type:"innerHTML", text:"<strong>Melde dich für unseren Newsletter an</strong> um im Gegenzug unsere Knochenwitze zu bekommen!"},		
 	"mce-EMAIL": {type:"placeholder", text:"Anmelden für Release Infos!"}	
 };
 
 var lang_en = {
 	lang_skelltext_1:       {type:"innerHTML", text:`
-Sometimes, when there is nobody up to the task, old heroes have to rise from the grave to save the day!
-Crush your enemies and make good use of their guts - literally, as you can augment Skippy with your
-foe's body parts and weapons!
-<br><br>
-Help Skippy, the cubic skeleton, to fight off Squaruman's endless forces of undead and vicious
-foes in this rectangular action packed cardboard adventure!
-<br><br>
-Skellboy is developed using Unity and its release is slated for mid 2019 for Windows,
-MacOS and Linux!
+					Sometimes, when there is nobody up to the task, old heroes have to <strong>rise from the grave</strong> to save the day!
+					Crush your enemies and make good use of their guts - literally, as you can <strong>augment Skippy with your
+					foe's body parts </strong> and weapons!
+					<br><br>
+					Help <strong>Skippy</strong>, the cubic skeleton, to fight off <strong>Squaruman's</strong> endless forces of undead and vicious
+					foes in this rectangular action packed cardboard adventure!
 	` },
 	lang_skelltext_release: {type:"innerHTML", text:"mid 2019"},
+	lang_skelltext_and: {type:"innerHTML", text:"and"},	
+	lang_skelltext_pub: {type:"innerHTML", text:"Switch version published by"},
+	lang_skelltext_follow: {type:"innerHTML", text:"Follow us!"},	
+	lang_skelltext_follow2: {type:"innerHTML", text:"You can join our <a href='https://discord.gg/8eWHk53'>discord</a> and share all of your <strong>bone jokes</strong> with us!"},	
+	lang_skelltext_follow3: {type:"innerHTML", text:"or"},		
+	lang_skelltext_follow4: {type:"innerHTML", text:"<strong>subscribe to our Newsletter</strong> to get all of our bone jokes!"},	
 	"mce-EMAIL": {type:"placeholder", text:"Subscribe for release news!"}		
 };
 
